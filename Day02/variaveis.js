e466884b32343d7ab38db51e34da097ac4928ee6
armanoalves <armanobarrosalvesh@gmail.com>
2023-03-14 12:48:03 -0300

    Desafio do dia 02

---
+++ Day02/variaveis.js
@@ -0,0 +1,5 @@
+nome = prompt("Qual o seu nome? ");
+idade = prompt("Quantos anos você tem? ");
+linguagem = prompt("Qual linguagem de programação você está estudando? ");
+
+document.write(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`);
